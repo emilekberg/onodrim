02d647539b90e10690009b1d0001a93a0945b908
Emil Ekberg <ekberg.emil@gmail.com>
2017-02-22 21:13:40 +0100

    Update build dependencies

--- gulp/rollup.js
+++ gulp/rollup.js
@@ -1,17 +1,11 @@
-const gulp = require('gulp');
-const rollup = require('rollup-stream');
-const source = require('vinyl-source-stream');
-const typescript = require('rollup-plugin-typescript');
+const rollup = require('rollup');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const string = require('rollup-plugin-string');
-const sourcemaps = require('gulp-sourcemaps');
-const buffer = require('vinyl-buffer');
 const rollupSourcemaps = require('rollup-plugin-sourcemaps');
 
-var cache;
+let cache;
 exports.bundle = function bundle() {
-    return rollup({
-        rollup: require('rollup'),
+    return rollup.rollup({
         entry: './bin/index.js',
         sourceMap: true,
         plugins: [
@@ -25,20 +19,18 @@ exports.bundle = function bundle() {
             nodeResolve({
                 main: true,
                 extensions: [
-                    '.ts', '.js'
+                    '.js'
                 ]
             })
-        ],
-        format: 'cjs',
-        moduleName: 'Onodrim',
-        //cache: cache
+        ]
     })
-    .on('bundle', function(bundle) {
-        //cache = bundle;
-    })
-    .pipe(source('onodrim.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist'))
+    .then((bundle) => {
+        cache = bundle;
+        bundle.write({
+            format: 'cjs',
+            moduleName: 'Onodrim',
+            dest: './dist/onodrim.js',
+            sourceMap: true
+        });
+    });
 }
